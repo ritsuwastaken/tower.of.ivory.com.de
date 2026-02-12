f2f5283708e9c0d6c1eae11c611435f871e8ef99
ritsuwastaken <discohijackat125ruemontmarte@proton.me>
2026-02-12 11:15:35 +0000

    Harbor 2.1 update

--- public/icon/script.sh
+++
@@ -1,8 +0,0 @@
-for f in *.JPG *.JPEG *.PNG *.GIF *.BMP *.WEBP; do
-  [ -e "$f" ] || continue
-  lower=$(echo "$f" | tr '[:upper:]' '[:lower:]')
-  if [ "$f" != "$lower" ]; then
-    tmp="__git_tmp__$$"
-    git mv "$f" "$tmp" && git mv "$tmp" "$lower"
-  fi
-done
