242544f2c0aa2fe516a78032d1afdc3c62611e92
ritsuwastaken <discohijackat125ruemontmarte@proton.me>
2026-02-26 09:40:16 +0000

    Revert "Add redirects for gh pages"
    This reverts commit 7087c5773034b71074f22d1a3be80a7c2195090a.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,9 @@
 import { Head } from '@/components/Head'
 
 export default function Custom404() {
-  const isGhPages = process.env.GITHUB_PAGES === '1'
-
   return (
     <>
       <Head description="Page Not Found" />
-      {isGhPages && (
-        <>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-  sessionStorage.redirect = location.href;
-              `,
-            }}
-          />
-          <meta httpEquiv="refresh" content="0;URL='/tower.of.ivory.com.de'" />
-        </>
-      )}
       <div
         style={{
           display: 'flex',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,10 @@ import { useEffect } from 'react'
 
 export default function IndexPage() {
   const router = useRouter()
-  const isGhPages = process.env.GITHUB_PAGES === '1'
 
   useEffect(() => {
     router.replace('/armorsets')
   }, [router])
 
-  return (
-    <>
-      {isGhPages && (
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-  (function(){
-    var redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-    if (redirect && redirect != location.href) {
-      history.replaceState(null, null, redirect);
-    }
-  })();
-            `,
-          }}
-        />
-      )}
-    </>
-  )
+  return null
 }
